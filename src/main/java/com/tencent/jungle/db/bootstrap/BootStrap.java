e5c324d4f588f0cb7e818bf080f8a963d633bc44
andy <416548283@qq.com>
2017-11-02 00:33:21 +0800

    测试使用

--- src/main/java/com/tencent/jungle/db/bootstrap/BootStrap.java
+++ src/main/java/com/tencent/jungle/db/bootstrap/BootStrap.java
@@ -1,7 +1,28 @@
 package com.tencent.jungle.db.bootstrap;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+import com.google.inject.Module;
+import com.tencent.jungle.db.JungleDatabaseModule;
+
 public class BootStrap {
 	public static void main(String[] args) {
 		System.out.println("hello,world");
+		List<Module> modules = loadModule();
+		Injector inject = Guice.createInjector(modules);
+		
+		//TODO--使用jungle db 去访问db
 	}
+	
+	private static List<Module> loadModule() {
+		List<Module> modules = new ArrayList<Module>();
+		modules.add(new JungleDatabaseModule());
+		return modules;
+	}
+	
+	
+	//TODO--新建一个mapper去访问本地db
 }
